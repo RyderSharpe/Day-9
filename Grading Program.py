053dd31453956a80229db8bb4aeda5a34e7ce42b
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-01-24 18:32:47 -0800

    Add files via upload

--- Grading Program.py
+++ Grading Program.py
@@ -5,11 +5,26 @@ student_scores = {
   "Draco": 74,
   "Neville": 62,
 }
+
 # 🚨 Don't change the code above 👆
 # TODO-1: Create an empty dictionary called student_grades.
 
+student_grades = {}
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
+
+for key in student_scores:
+    #print(key) # prints Harry
+    grade = (student_scores[key]) # prints 81
+
+    if 91 <= grade <= 100:
+        student_grades[key] = "Outstanding"
+    elif 81 <= grade <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif 71 <= grade <= 80:
+        student_grades[key] = "Acceptable"
+    elif grade <= 70:
+        student_grades[key] = "Fail"
 
 
 # 🚨 Don't change the code below 👇
