a6b16e6b442f910897d974133ca99271585f0ede
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-01-24 21:59:52 -0800

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,26 @@
+dict = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+
+# dict["c"] = [1,2,3]
+# print(dict)
+
+########################################
+
+# for key in dict:
+#     dict[key] += 1
+#     print(dict)
+
+########################################
+
+# dict[1] = 4
+# print(dict)
+
+########################################
+#
+# print(dict[1])
+# # There is no key that matches 1, this will give you a KeyError.
+# # Unlike with a Python List, using the [1] square bracket notation on a dictionary will *not* retrieve the second item.
+
