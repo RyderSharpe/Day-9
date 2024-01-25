4bb948003eb407b99d4118f4d19758e37ef8aa9b
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-01-24 18:35:47 -0800

    Add files via upload

--- Grading Program.py
+++ Grading Program.py
@@ -13,18 +13,18 @@ student_grades = {}
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
 
-for key in student_scores:
-    #print(key) # prints Harry
-    grade = (student_scores[key]) # prints 81
+for student in student_scores:
+    #print(student) # prints Harry
+    grade = (student_scores[student]) # prints 81
 
     if 91 <= grade <= 100:
-        student_grades[key] = "Outstanding"
+        student_grades[student] = "Outstanding"
     elif 81 <= grade <= 90:
-        student_grades[key] = "Exceeds Expectations"
+        student_grades[student] = "Exceeds Expectations"
     elif 71 <= grade <= 80:
-        student_grades[key] = "Acceptable"
+        student_grades[student] = "Acceptable"
     elif grade <= 70:
-        student_grades[key] = "Fail"
+        student_grades[student] = "Fail"
 
 
 # 🚨 Don't change the code below 👇
